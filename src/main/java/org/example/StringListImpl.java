733523264b8a58e46359cab9cd222e847fb5d222
Cranky Bawler <6666lenia6666@mail.ru>
2024-12-16 19:36:54 +0300

    1

---
+++ src/main/java/org/example/StringListImpl.java
@@ -0,0 +1,106 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class StringListImpl implements StringList {
+    private String[] items;
+    private int size;
+
+    public StringListImpl() {
+        this.items = new String[10];
+        this.size = 0;
+    }
+
+    @Override
+
+    public String add(String item) {
+        if (size == items.length) {
+            items = Arrays.copyOf(items, items.length * 2);
+        }
+        items[size] = item;
+        size++;
+        return item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Такого индекса массива не существует" + index);
+        }
+        if (size == items.length) {
+            items = Arrays.copyOf(items, items.length * 2);
+        }
+        System.arraycopy(items, index, items, index + 1, size - index);
+        items[index] = item;
+        size++;
+        return item;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+
+    public String[] getItems() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/test/java/StringListImplTest.java
@@ -0,0 +1,58 @@
+import org.example.StringListImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringListImplTest {
+    private StringListImpl stringListImpl;
+
+    @BeforeEach
+    public void setUp() {
+        stringListImpl = new StringListImpl();
+    }
+
+    @Test
+    public void add() {
+
+        String item1 = "Item1";
+        String item2 = "Item2";
+        String item3 = "Item3";
+
+        stringListImpl.add(item1);
+        stringListImpl.add(item2);
+        String result = stringListImpl.add(item3);
+
+        // Проверяем, что возвращенный элемент равен добавленному
+        assertEquals(item3, result);
+
+        //Проверяем, что все элементы действительно добавлены в массив
+        String[] items = stringListImpl.getItems();
+
+        assertEquals(3, items.length);
+        assertEquals(item1, items[0]);
+        assertEquals(item2, items[1]);
+        assertEquals(item3, items[2]);
+    }
+
+    @Test
+    public void addForIndex() {
+
+        stringListImpl.add(0, "Item1");
+        stringListImpl.add(1, "Item2");
+        String item = "TestItem";
+        String result = stringListImpl.add(1, item);
+
+        assertEquals(item, result);
+        String[] items = stringListImpl.getItems();
+        assertEquals(3, items.length);
+        assertEquals("Item1", items[0]);
+        assertEquals(item, items[1]);
+        assertEquals("Item2", items[2]);
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            stringListImpl.add(15, "TestItem");
+        });
+
+    }
+}
